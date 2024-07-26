b6fed88551575a05d5a1727af6ff85fe764401a0
Munesh Chandravanshi <145778515+Chandra-25s@users.noreply.github.com>
2024-07-27 05:25:32 +0530

    My first project

---
+++ hms/faculty/include/sidebar.php
@@ -0,0 +1,157 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span><b>Main Navigation</b></span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								
+							
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Students Logs </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> Manage Students </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Students </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-patient.php">
+											<span class="title"> Manage Students </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Appointment History </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Conatct us Queries </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="unread-queries.php">
+											<span class="title"> Unread Query </span>
+										</a>
+									</li>
+
+									<li>
+										<a href="read-query.php">
+											<span class="title"> Read Query </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+
+
+	<li>
+								
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Reports </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="between-dates-reports.php">
+											<span class="title">B/w dates reports </span>
+										</a>
+									</li>
+
+									
+									
+								</ul>
+								<li>
+								<a href="patient-search.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-search"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Students Search </span>
+										</div>
+									</div>
+								</a>
+							</li>
+								</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
---
+++ hms/user-login.php
@@ -0,0 +1,179 @@
+<?php
+session_start();
+error_reporting(0);
+include("include/config.php");
+if(isset($_POST['submit']))
+{
+$ret=mysqli_query($con,"SELECT * FROM users WHERE email='".$_POST['username']."' and password='".md5($_POST['password'])."'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$extra="dashboard.php";//
+$_SESSION['login']=$_POST['username'];
+$_SESSION['id']=$num['id'];
+$host=$_SERVER['HTTP_HOST'];
+$uip=$_SERVER['REMOTE_ADDR'];
+$status=1;
+// For stroing log if user login successfull
+$log=mysqli_query($con,"insert into userlog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['login']."','$uip','$status')");
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+else
+{
+	// For stroing log if user login unsuccessfull
+$_SESSION['login']=$_POST['username'];	
+$uip=$_SERVER['REMOTE_ADDR'];
+$status=0;
+mysqli_query($con,"insert into userlog(username,userip,status) values('".$_SESSION['login']."','$uip','$status')");
+$_SESSION['errmsg']="Invalid username or password";
+$extra="user-login.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>User-Login</title>
+		
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+	</head>
+	<body class="login">
+	
+		<div class="row" >
+			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+				<div class="logo margin-top-30">
+				<a href="../index.html"><h2 style="text-align:center"> <b>DUDMS | Patient Login</b></h2></a><hr>
+				</div>
+
+				<div class="box-login">
+					<form class="form-login" method="post">
+						<fieldset>
+							<legend>
+								Sign in to your account
+							</legend>
+							<p>
+								Please enter your name and password to log in.<br />
+								<span style="color:red;"><?php echo $_SESSION['errmsg']; ?><?php echo $_SESSION['errmsg']="";?></span>
+							</p>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="text" class="form-control" name="username" placeholder="Username">
+									<i class="fa fa-user"></i> </span>
+							</div>
+							<div class="form-group form-actions">
+								<span class="input-icon">
+									<input type="password" class="form-control password" name="password" placeholder="Password">
+									<i class="fa fa-lock"></i>
+									 </span><a href="forgot-password.php">
+									Forgot Password ?
+								</a>
+							</div>
+							<div id="captchaContainer" style="border:1px solid black; height: 20px; width:150px;"></div>
+							<button onclick="refreshCaptcha()">Refresh CAPTCHA</button>
+							<div class="form-actions">
+							<input type="text" placeholder="ENTER THE CAPTCHA"style="border:1px solid black; height: 20px; width:150px;">
+  							
+								
+								<button type="submit" class="btn btn-primary pull-right" name="submit">
+									Login <i class="fa fa-arrow-circle-right"></i>
+								</button>
+							</div>
+							<div class="new-account">
+								Don't have an account yet?
+								<a href="registration.php">
+									Create an account
+								</a>
+							</div>
+						</fieldset>
+					</form>
+
+					<div class="copyright">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> DUDMS</span>. <span>Tech Titans</span>
+					</div>
+			
+				</div>
+
+			</div>
+		</div>
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+	
+		<script src="assets/js/main.js"></script>
+
+		<script src="assets/js/login.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				Login.init();
+			});
+		</script>
+		<script>
+        // Function to generate a random 4-digit number
+        function generateCaptcha(length) {
+  var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var captcha = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    captcha += charset[randomIndex];
+  }
+  return captcha;
+}
+
+function displayCaptcha(containerId, length) {
+  var captcha = generateCaptcha(length);
+  var captchaContainer = document.getElementById(containerId);
+  captchaContainer.textContent = captcha;
+}
+
+function refreshCaptcha() {
+  displayCaptcha("captchaContainer", 6); // Change 6 to adjust the length of the CAPTCHA
+}
+
+// Initial display
+displayCaptcha("captchaContainer", 6);
+
+        // Function to validate CAPTCHA
+        function validateCaptcha() {
+            var userInput = document.getElementById('captchaInput').value;
+            var captchaText = document.getElementById('captcha').textContent;
+
+            if (userInput === captchaText) {
+                alert('CAPTCHA is correct!');
+            } else {
+                alert('CAPTCHA is incorrect. Please try again.');
+                // Refresh CAPTCHA
+                displayCaptcha();
+            }
+        }
+
+        // Display CAPTCHA on page load
+        window.onload = function() {
+            displayCaptcha();
+        };
+    </script>
+	
+	</body>
+	<!-- end: BODY -->
+</html>
